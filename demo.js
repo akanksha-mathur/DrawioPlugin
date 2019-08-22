9d454af8e1354087a960e0e49e0ae944a8675a49
akanksha-mathur <54259914+akanksha-mathur@users.noreply.github.com>
2019-08-22 15:32:55 +0530

    Create demo.js

---
+++ demo.js
@@ -0,0 +1,137 @@
+/**
+ * Sample plugin.
+ */
+Draw.loadPlugin(function(ui) {
+
+	var graph = ui.editor.graph;
+	var enabled = true;
+	var counter = 0;
+	
+	// Creates the shape for the shape number and puts it into the draw pane
+    var redrawShape = graph.cellRenderer.redrawShape;
+//     alert("Initial");
+	graph.cellRenderer.redrawShape = function(state, force, rendering)
+	{
+		var result = redrawShape.apply(this, arguments);
+
+		if (result && enabled && graph.model.isVertex(state.cell))
+		{
+			if (state.shape != null && state.secondLabel == null)
+			{	
+				
+				switch(state.style[mxConstants.STYLE_SHAPE]){
+                case 'mxgraph.pid.pumps.centrifugal_pump_1':
+                        var doc = mxUtils.createXmlDocument();
+                        var node = doc.createElement('MyNode')
+                        node.setAttribute('Pressure Increase (kgf/cm2)', '2');
+                        node.setAttribute('Outlet Pressure (kgf/cm2)', '4');
+                        node.setAttribute('Efficiency (%)', '60');
+                        node.setAttribute('Power (KW)', '52');
+                        state.cell.value=node;
+                        var controllabel='Pump';
+                        break;
+
+                        case 'mxgraph.pid.vessels.container,_tank,_cistern':
+                                var doc = mxUtils.createXmlDocument();
+                                var node = doc.createElement('MyNode')
+                                node.setAttribute('Pressure Calculation Mode', 'Inlet Minimum');
+                                state.cell.value=node;
+                                var controllabel='Tank';
+                        break;
+                        case 'mxgraph.pid.misc.mixer':
+                                var doc = mxUtils.createXmlDocument();
+                                var node = doc.createElement('MyNode')
+                                node.setAttribute('Volume (m3)', '900');
+                                state.cell.value=node;
+                                var controllabel='Mixer';
+                        break;
+                }
+						
+				
+                var value = '<div style="padding:2px;border:1px solid gray;background:yellow;border-radius:2px;">' + controllabel +': '+(++counter) + '</div>';
+//                 alert("counter"+counter);
+				state.secondLabel = new mxText(value, new mxRectangle(),
+                        mxConstants.ALIGN_LEFT, mxConstants.ALIGN_BOTTOM);
+                        
+
+				// Styles the label
+				state.secondLabel.size = 12;
+				state.secondLabel.dialect = state.shape.dialect;
+				state.secondLabel.dialect = mxConstants.DIALECT_STRICTHTML;
+				graph.cellRenderer.initializeLabel(state, state.secondLabel);
+			}
+		}
+		
+		if (state.secondLabel != null)
+		{
+			var scale = graph.getView().getScale();
+			var bounds = new mxRectangle(state.x + state.width - 4 * scale, state.y + 4 * scale, 0, 0);
+			state.secondLabel.state = state;
+			state.secondLabel.scale = scale;
+			state.secondLabel.bounds = bounds;
+			state.secondLabel.redraw();
+		}
+		
+		return result;
+	};
+
+	// Destroys the shape number
+	var destroy = graph.cellRenderer.destroy;
+	graph.cellRenderer.destroy = function(state)
+	{
+		destroy.apply(this, arguments);
+		
+		if (state.secondLabel != null)
+		{
+			state.secondLabel.destroy();
+			state.secondLabel = null;
+		}
+	};
+	
+	graph.cellRenderer.getShapesForState = function(state)
+	{	
+// 	 	alert(state.shape);
+// 		alert(state.text);
+// 		alert(state.secondLabel);
+// 		alert(state.control);
+// 		alert(state.style);
+// 		alert(state.style[mxConstants.STYLE_SHAPE]);
+		return [state.shape, state.text, state.secondLabel, state.control];
+	};
+	
+	var validate = graph.view.validate;
+	graph.view.validate = function()
+	{
+		counter = 0;
+		validate.apply(this, arguments);
+	};
+	
+	// Extends View menu
+	mxResources.parse('number=Number');
+
+    // Adds action
+    var action = ui.actions.addAction('number...', function()
+    {
+		enabled = !enabled;
+		graph.refresh();
+    });
+	
+    action.setToggleAction(true);
+	action.setSelectedCallback(function() { return enabled; });
+    
+	var menu = ui.menus.get('view');
+	var oldFunct = menu.funct;
+	
+	menu.funct = function(menu, parent)
+	{
+		oldFunct.apply(this, arguments);
+		
+		ui.menus.addMenuItems(menu, ['-', 'number'], parent);
+	};
+	
+	// Forces refresh if file was loaded before plugin
+	if (ui.getCurrentFile() != null)
+	{
+		graph.refresh();
+	}
+});
