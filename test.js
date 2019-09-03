4092133855dde8ff63d1f7fa562293e83502f097
akanksha-mathur <54259914+akanksha-mathur@users.noreply.github.com>
2019-09-03 10:38:48 +0530

    Update test.js

--- test.js
+++ test.js
@@ -4,7 +4,7 @@
  xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-    alert(this.responseText);
+    alert(" tyr");
     }
   };
   xhttp.open("GET", "https://www.quora.com/How-can-I-run-my-PHP-content-on-GitHub-pages", true);
